e170de226cc82e7ac64f04b818f90b326cdb210b
ChathumikaSitecore <ctlw@sitecore.net>
2025-09-02 17:34:00 +0530

    PageHeaderSt undefined title fix

--- examples/kit-nextjs-product-listing/src/components/site-three/PageHeaderST.tsx
+++ examples/kit-nextjs-product-listing/src/components/site-three/PageHeaderST.tsx
@@ -30,7 +30,7 @@ export const Default = (props: PageHeaderSTProps) => {
     >
       <div className="absolute inset-0 z-10">
         <ContentSdkImage
-          field={props.fields.Image}
+          field={props.fields?.Image}
           width={1920}
           height={1080}
           className="w-full h-full object-cover"
@@ -39,10 +39,10 @@ export const Default = (props: PageHeaderSTProps) => {
       <div className="relative container px-4 mx-auto z-20">
         <div className="grid gap-12 items-center w-full lg:grid-cols-2">
           <h1 className="text-primary text-4xl lg:text-7xl">
-            <ContentSdkText field={props.fields.Title} />
+            <ContentSdkText field={props.fields?.Title} />
           </h1>
           <div className="text-white text-xl lg:text-3xl font-medium">
-            <ContentSdkRichText field={props.fields.Body} />
+            <ContentSdkRichText field={props.fields?.Body} />
           </div>
         </div>
       </div>
@@ -62,7 +62,7 @@ export const TextRight = (props: PageHeaderSTProps) => {
     >
       <div className="absolute inset-0 z-10">
         <ContentSdkImage
-          field={props.fields.Image}
+          field={props.fields?.Image}
           width={1920}
           height={1080}
           className="w-full h-full object-cover"
@@ -71,10 +71,10 @@ export const TextRight = (props: PageHeaderSTProps) => {
       <div className="relative container px-4 mx-auto z-20">
         <div className="grid gap-12 max-w-[36rem]">
           <h1 className="text-primary text-4xl lg:text-7xl">
-            <ContentSdkText field={props.fields.Title} />
+            <ContentSdkText field={props.fields?.Title} />
           </h1>
           <div className="text-white text-xl lg:text-3xl font-medium">
-            <ContentSdkRichText field={props.fields.Body} />
+            <ContentSdkRichText field={props.fields?.Body} />
           </div>
         </div>
       </div>
@@ -94,7 +94,7 @@ export const SplitScreen = (props: PageHeaderSTProps) => {
     >
       <div className="w-full mb-18 lg:absolute lg:inset-0 lg:mb-0 lg:z-10">
         <ContentSdkImage
-          field={props.fields.Image}
+          field={props.fields?.Image}
           width={1920}
           height={1080}
           className="w-full h-[20rem] lg:h-full object-cover lg:w-1/2 lg:ml-auto"
@@ -103,10 +103,10 @@ export const SplitScreen = (props: PageHeaderSTProps) => {
       <div className="relative container px-4 mx-auto z-20">
         <div className="grid gap-8 lg:max-w-[50%] pr-8">
           <h1 className="text-foreground text-4xl lg:text-7xl">
-            <ContentSdkText field={props.fields.Title} />
+            <ContentSdkText field={props.fields?.Title} />
           </h1>
           <div className="text-foreground text-xl lg:text-3xl font-medium">
-            <ContentSdkRichText field={props.fields.Body} />
+            <ContentSdkRichText field={props.fields?.Body} />
           </div>
         </div>
       </div>
@@ -124,10 +124,10 @@ export const Stacked = (props: PageHeaderSTProps) => {
       <div className="container px-4 mx-auto">
         <div className="grid gap-x-12 gap-y-8 items-center w-full lg:grid-cols-2">
           <h1 className="text-4xl lg:text-7xl">
-            <ContentSdkText field={props.fields.Title} />
+            <ContentSdkText field={props.fields?.Title} />
           </h1>
           <div className="text-xl lg:text-3xl font-medium">
-            <ContentSdkRichText field={props.fields.Body} />
+            <ContentSdkRichText field={props.fields?.Body} />
           </div>
         </div>
       </div>
@@ -152,14 +152,14 @@ export const TwoColumn = (props: PageHeaderSTProps) => {
         <div className="grid gap-x-12 gap-y-8 w-full lg:grid-cols-2">
           <div>
             <h1 className="text-4xl lg:text-7xl mb-8">
-              <ContentSdkText field={props.fields.Title} />
+              <ContentSdkText field={props.fields?.Title} />
             </h1>
             <div className="text-xl lg:text-3xl font-medium">
-              <ContentSdkRichText field={props.fields.Body} />
+              <ContentSdkRichText field={props.fields?.Body} />
             </div>
           </div>
           <ContentSdkImage
-            field={props.fields.Image}
+            field={props.fields?.Image}
             width={1080}
             height={1080}
             className="w-full h-full aspect-3/2 object-cover lg:aspect-square"
